e5e7190e52e74250f0e5bdfdc97cf9d3932c076e
guowei42 <65711401+guowei42@users.noreply.github.com>
2023-12-30 16:59:19 +0800

    abstract numpad

--- src/components/Bounceculator/components/NumPad.tsx
+++ src/components/Bounceculator/components/NumPad.tsx
@@ -1,73 +1,60 @@
 import { Button } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
-import { useState } from "react";
 
-const NumPad = () => {
-    const [calcVal, setCalcVal] = useState<number>(0);
+const numPadData = [
+    "C",
+    "AC",
+    "%",
+    "/",
+    "7",
+    "8",
+    "9",
+    "x",
+    "4",
+    "5",
+    "6",
+    "-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "0",
+    ".",
+    "=",
+];
+
+const NumPad = ({
+    handleCalcVal,
+    handleReset,
+}: {
+    handleCalcVal: (num: number) => void;
+    handleReset: () => void;
+}) => {
+    const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const value = event.currentTarget.textContent;
+        if (!value) {
+            return;
+        }
+        if (value == "C") {
+            handleReset();
+        } else if (!isNaN(+value)) {
+            handleCalcVal(Number(value));
+        }
+    };
 
     return (
         <Grid container spacing={1}>
-            <Grid xs={3}>
-                <Button>C</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>AC</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>%</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>/</Button>
-            </Grid>
-
-            <Grid xs={3}>
-                <Button>7</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>8</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>9</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>x</Button>
-            </Grid>
-
-            <Grid xs={3}>
-                <Button>4</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>5</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>6</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>-</Button>
-            </Grid>
-
-            <Grid xs={3}>
-                <Button>1</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>2</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>3</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>+</Button>
-            </Grid>
-
-            <Grid xs={6}>
-                <Button>0</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>.</Button>
-            </Grid>
-            <Grid xs={3}>
-                <Button>=</Button>
-            </Grid>
+            {numPadData.map((val) =>
+                val === "0" ? (
+                    <Grid xs={6}>
+                        <Button onClick={handleButtonClick}>{val}</Button>
+                    </Grid>
+                ) : (
+                    <Grid xs={3}>
+                        <Button onClick={handleButtonClick}>{val}</Button>
+                    </Grid>
+                )
+            )}
         </Grid>
     );
 };
--- src/components/Bounceculator/index.tsx
+++ src/components/Bounceculator/index.tsx
@@ -5,6 +5,21 @@ import NumPad from "./components/NumPad";
 
 const Bounceculator = () => {
     const [calcVal, setCalcVal] = useState<number>(0);
+    const [isReset, setIsReset] = useState<boolean>(true);
+
+    const handleCalcVal = (num: number) => {
+        if (isReset) {
+            setCalcVal(num);
+            setIsReset(false);
+        } else {
+            setCalcVal(calcVal * 10 + num);
+        }
+    };
+
+    const handleReset = () => {
+        setIsReset(true);
+        setCalcVal(0);
+    };
 
     return (
         <Container
@@ -17,7 +32,7 @@ const Bounceculator = () => {
         >
             <Box sx={{ border: "1px solid black" }}>
                 <CalcDisplay value={calcVal} />
-                <NumPad />
+                <NumPad handleCalcVal={handleCalcVal} handleReset={handleReset} />
             </Box>
         </Container>
     );
